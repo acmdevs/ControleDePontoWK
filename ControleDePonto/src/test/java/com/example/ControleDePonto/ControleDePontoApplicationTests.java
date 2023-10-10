3354ea112cbe621d1b62c50faac6a4f1241b2f87
Alvaro Santos <alvaro_rosario@outlook.com>
2023-10-09 23:41:33 -0300

    Criado estrutura inicial do projeto

---
+++ ControleDePonto/src/test/java/com/example/ControleDePonto/ControleDePontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleDePonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDePontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
